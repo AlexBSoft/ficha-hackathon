4db83c9cbb9085f4c515a3036db679cbace98ae0
user <user>
2023-08-26 08:06:59 +0300

    rename to PayDog

--- app/page.tsx
+++ app/page.tsx
@@ -66,13 +66,13 @@ export default async function Index() {
         <Link href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span className="text-xs bg-purple-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> 
             <span className="text-sm font-medium">
-              Посмотрите видео о DogPay</span> 
+              Посмотрите видео о PayDog</span> 
             <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </Link>
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Платежная платформа</h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-          <b>DogPay</b> это супер крутая платформа цифровых валют, позволяющая оформлять карты в зарубежных банках</p>
+          <b>PayDog</b> это супер крутая платформа цифровых валют, позволяющая оформлять карты в зарубежных банках</p>
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <Link href="/login" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Создать аккаунт
